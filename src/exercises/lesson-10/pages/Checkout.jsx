bb456ec912ddfdb55da8e07bb533de02992fca98
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-06-06 17:26:32 -0700

    Implement useNavigate in Checkout page

--- src/exercises/lesson-10/pages/Checkout.jsx
+++ src/exercises/lesson-10/pages/Checkout.jsx
@@ -1,19 +1,27 @@
 import { useNavigate } from 'react-router-dom';
 
 export default function Checkout() {
-  const navigate = null;
+  const navigate = useNavigate();
 
-  function handleGoHome() {}
+  function handleGoHome() {
+    navigate('/');
+  }
 
-  function handleBack() {}
+  function handleBack() {
+    navigate(-1);
+  }
 
   return (
     <section>
       <h2>Checkout</h2>
-      <p>This page exists to practice useNavigate().</p>
+
+      <p>
+        This page exists to practice <code>useNavigate()</code>.
+      </p>
 
       <div style={{ display: 'flex', gap: 10 }}>
         <button onClick={handleGoHome}>Go Home (navigate)</button>
+
         <button onClick={handleBack}>Back (navigate -1)</button>
       </div>
     </section>
