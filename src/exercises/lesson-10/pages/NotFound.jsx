926bde9f543317c6358773555db8e0fff8549d53
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-06-06 17:27:19 -0700

    Complete Lesson 10 React Router exercises

--- src/exercises/lesson-10/pages/NotFound.jsx
+++ src/exercises/lesson-10/pages/NotFound.jsx
@@ -1,11 +1,19 @@
 import { Link, useLocation } from 'react-router-dom';
 
 export default function NotFound() {
-  const pathname = '';
+  const location = useLocation();
 
   return (
     <section>
       <h2>404: Not Found</h2>
+
+      <p>
+        Invalid path: <code>{location.pathname}</code>
+      </p>
+
+      <div style={{ marginTop: 12 }}>
+        <Link to="/">Go Home</Link>
+      </div>
     </section>
   );
 }
